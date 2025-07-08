85539c0ed423f51d22390616c6e1ce9de3c5bb03
tmfrl99 <93257321+tmfrl99@users.noreply.github.com>
2025-07-08 14:36:47 +0900

    [Silver V] Title: 행렬 곱셈, Time: 168 ms, Memory: 16356 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int na = Integer.parseInt(st.nextToken());
+		int ma = Integer.parseInt(st.nextToken());
+
+		int[][] arrA = new int[na][ma];
+		for (int i = 0; i < na; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < ma; j++)
+				arrA[i][j] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		int nb = Integer.parseInt(st.nextToken());
+		int mb = Integer.parseInt(st.nextToken());
+
+		int[][] arrB = new int[nb][mb];
+		for (int i = 0; i < nb; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < mb; j++)
+
+				arrB[i][j] = Integer.parseInt(st.nextToken());
+		}
+
+		int[][] res = new int[na][mb];
+		int sum;
+		for (int i = 0; i < na ; i++) {
+			for (int j = 0; j < mb; j++) {
+				sum = 0;
+				for (int k = 0; k < ma; k++) {
+					sum += arrA[i][k] * arrB[k][j];
+				}
+				res[i][j] = sum;
+				sb.append(res[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
