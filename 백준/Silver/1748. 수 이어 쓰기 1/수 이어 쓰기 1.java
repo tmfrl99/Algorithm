bde3075c09bdc7a93ff2c00fa617e04b7b9bd07b
tmfrl99 <93257321+tmfrl99@users.noreply.github.com>
2025-07-23 14:58:52 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -5,39 +5,15 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
-		int result = 0;
+		int digits = String.valueOf(n).length();
+		long answer = 0;
 
-		for (int i = 1; i <= n; i++) {
-			if (i < 10) {
-				result += 1;
-			}
-			else if (i < 100) {
-				result += 2;
-			}
-			else if (i < 1000) {
-				result += 3;
-			}
-			else if (i < 10000) {
-				result += 4;
-			}
-			else if (i < 100000) {
-				result += 5;
-			}
-			else if (i < 1000000) {
-				result += 6;
-			}
-			else if (i < 10000000) {
-				result += 7;
-			}
-			else if (i < 100000000) {
-				result += 8;
-			}
-			else {
-				result += 9;
-			}
-		}
+		for (int i = 1; i < digits; i++)
+			answer += 9L * (long)Math.pow(10, i - 1) * i;
 
-		bw.write(String.valueOf(result));
+		answer += (long)(n - (int)Math.pow(10, digits - 1) + 1) * digits;
+		
+        bw.write(String.valueOf(answer));
 		bw.flush();
 		bw.close();
 		br.close();
