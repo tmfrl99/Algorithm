ee03248f60ac3f290b3f3da23109a754579cf3be
tmfrl99 <93257321+tmfrl99@users.noreply.github.com>
2025-08-29 13:22:16 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 224 ms, Memory: 27160 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -2,35 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		String game = st.nextToken();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        char gameType = st.nextToken().charAt(0);
 
-		Set<String> set = new HashSet<>();
-		for (int i = 0; i < N; i++) {
-			String name = br.readLine();
-			set.add(name);
-		}
+        Set<String> players = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            players.add(br.readLine());
+        }
 
-		int result = 0;
-		switch (game) {
-			case "Y":
-				result = set.size();
-				break;
-			case "F":
-				result = set.size() / 2;
-				break;
-			case "O":
-				result = set.size() / 3;
-				break;
-		}
+        int required = 0;
+        if (gameType == 'Y') required = 2;
+        else if (gameType == 'F') required = 3;
+        else if (gameType == 'O') required = 4;
 
-		bw.write(String.valueOf(result));
-		bw.flush();
+        bw.write(String.valueOf(players.size() / (required - 1)));
+        bw.flush();
 		bw.close();
 		br.close();
-	}
+    }
 }
