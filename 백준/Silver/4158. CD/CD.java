da758650b2076e9af3bf2044cc2d4c07f1f93f7e
tmfrl99 <93257321+tmfrl99@users.noreply.github.com>
2025-09-03 13:52:48 +0900

    [Silver V] Title: CD, Time: 876 ms, Memory: 276856 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.java
+++ 백준/Silver/4158. CD/CD.java
@@ -13,21 +13,36 @@ public class Main {
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
 			if (N == 0 && M == 0) break;
+			int cnt = 0;
 
-			Set<Integer> cdNum = new HashSet<>();
+			int[] sang = new int[N];
+			int[] sun = new int[M];
+
 			for (int i = 0; i < N; i++) {
-				cdNum.add(Integer.parseInt(br.readLine()));
+				sang[i] = Integer.parseInt(br.readLine());
 			}
 
-			int cnt = 0;
-			for (int i = 0; i < M; i++) {
-				if (cdNum.contains(Integer.parseInt(br.readLine()))) {
+			for (int i = 0; i < N; i++) {
+				sun[i] = Integer.parseInt(br.readLine());
+			}
+
+			int p1 = 0;
+			int p2 = 0;
+
+			while (p1 < N && p2 < M) {
+				if (sang[p1] == sun[p2]) {
 					cnt++;
+					p1++;
+					p2++;
+				} else if (sang[p1] < sun[p2]) {
+					p1++;
+				} else {
+					p2++;
 				}
 			}
 			sb.append(cnt).append("\n");
 		}
-		
+
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
